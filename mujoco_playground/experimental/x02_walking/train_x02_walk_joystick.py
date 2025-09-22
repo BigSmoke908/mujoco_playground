9374f70141e665f03a4734d7d328ff5ec6c29f97
Jasper Güldenstein <jasper.gueldenstein@uni-hamburg.de>
2025-09-22 10:05:21 +0200

    render video

--- mujoco_playground/experimental/x02_walking/train_x02_walk_joystick.py
+++ mujoco_playground/experimental/x02_walking/train_x02_walk_joystick.py
@@ -8,18 +8,23 @@ import datetime
 from datetime import datetime
 import functools
 import os
+import mujoco
+import mediapy as media
 from brax.training.agents.ppo import networks as ppo_networks
 from brax.training.agents.ppo import train as ppo
 #from brax.training.agents.sac import networks as sac_networks
 #from brax.training.agents.sac import train as sac
 from etils import epath
 import jax
+from jax import numpy as jp
 from matplotlib import pyplot as plt
 import numpy as np
 from argparse import ArgumentParser
 import pickle
 from tqdm import tqdm
 from mujoco_playground.experimental.x02_walking.convert_to_onnx import conv_to_onnx
+from mujoco_playground._src.gait import draw_joystick_command
+
 
 def parse_kv(s):
     key, value = s.split("=")
@@ -58,7 +63,7 @@ from mujoco_playground.config import locomotion_params
 np.set_printoptions(precision=3, suppress=True, linewidth=100)
 
 env_name = args.env
-env = registry.load(env_name)
+env = registry.load(env_name, config_overrides=config_overrides)
 env_cfg = registry.get_default_config(env_name)
 ppo_params = locomotion_params.brax_ppo_config(env_name)
 
@@ -146,6 +151,81 @@ make_inference_fn, params, metrics = train_fn(
 )
 print(f"time to jit: {times[1] - times[0]}")
 print(f"time to train: {times[-1] - times[1]}")
+
+print("Rendering Video")
+
+eval_env = registry.load(env_name)
+jit_reset = jax.jit(eval_env.reset)
+jit_step = jax.jit(eval_env.step)
+jit_inference_fn = jax.jit(make_inference_fn(params, deterministic=True))
+
+rng = jax.random.PRNGKey(1)
+
+rollout = []
+modify_scene_fns = []
+
+commands = jp.array([[0.0, 0.0, 0.0],
+                     [1.0, 0.0, 0.0],
+                     [-0.5, 0.0, 0.0],
+                     [0.0, 0.4, 0.0],
+                     [0.0, -0.4, 0.0],
+                     [0.0, 0.0, 2.0],
+                     [0.0, 0.0, -2.0],])
+phase_dt = 2 * jp.pi * eval_env.dt * 1.5
+phase = jp.array([0, jp.pi])
+
+for j in range(commands.shape[0]):
+  print(f"episode {j}")
+  state = jit_reset(rng)
+  state.info["phase_dt"] = phase_dt
+  state.info["phase"] = phase
+  for i in range(env_cfg.episode_length):
+    act_rng, rng = jax.random.split(rng)
+    ctrl, _ = jit_inference_fn(state.obs, act_rng)
+    state = jit_step(state, ctrl)
+    if state.done:
+      break
+    state.info["command"] = commands[j]
+    rollout.append(state)
+
+    xyz = np.array(state.data.xpos[eval_env.mj_model.body("pelvis_link").id])
+    xyz += np.array([0, 0.0, 0])
+    x_axis = state.data.xmat[eval_env._torso_body_id, 0]
+    yaw = -np.arctan2(x_axis[1], x_axis[0])
+    modify_scene_fns.append(
+        functools.partial(
+            draw_joystick_command,
+            cmd=state.info["command"],
+            xyz=xyz,
+            theta=yaw,
+            scl=1.0,
+        )
+    )
+
+render_every = 1
+fps = 1.0 / eval_env.dt / render_every
+print(f"fps: {fps}")
+traj = rollout[::render_every]
+mod_fns = modify_scene_fns[::render_every]
+
+scene_option = mujoco.MjvOption()
+scene_option.geomgroup[2] = True
+scene_option.geomgroup[3] = False
+scene_option.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
+scene_option.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = False
+scene_option.flags[mujoco.mjtVisFlag.mjVIS_PERTFORCE] = False
+
+frames = eval_env.render(
+    traj,
+    camera="track",
+    scene_option=scene_option,
+    width=640,
+    height=480*2,
+    modify_scene_fns=mod_fns,
+)
+media.write_video(ckpt_path / f"{args.run_name}_eval.mp4", frames, fps=fps)
+if args.wandb:
+    wandb.log({"video": wandb.Video(str(ckpt_path / f"{args.run_name}_eval.mp4"), fps=fps, format="mp4")})
 
 save_params(ckpt_path, params)
 conv_to_onnx(ckpt_path / "params.pkl", f"{args.run_name}.onnx", env_name)

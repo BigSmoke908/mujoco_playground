210e5f3ddc9e5d0c95d1a31fc3cbeb886847d53e
Blitzhummel <yassin@tarchouni.de>
2025-11-30 17:11:36 +0100

    Update convert_urdf.py

--- mujoco_playground/_src/locomotion/wolves_op/xmls/convert_urdf.py
+++ mujoco_playground/_src/locomotion/wolves_op/xmls/convert_urdf.py
@@ -1,14 +1,16 @@
 import os
-from dm_control import mjcf
+from dm_control.mjcf.urdf import parser
 
 input_file = "humanoid_v49.urdf"
 output_file = "humanoid_converted.xml"
 
-# Konvertierung
 print(f"Konvertiere {input_file} → {output_file} ...")
-model = mjcf.from_file(input_file)
 
-# Speichern
+# URDF -> MJCF Konvertierung
+converter = parser.UrdfToMjcf()
+model = converter.parse(input_file)
+
+# Als XML speichern
 with open(output_file, "w") as f:
     f.write(model.to_xml_string())
 

2a0592fdf4ea907d0bd54392ef9d710fdbc6bda2
emeier <em.meier@ostfalia.de>
2025-12-11 21:08:42 +0100

    .

--- mujoco_playground/experimental/sim2sim/play_wolvesOP_joystick.py
+++ mujoco_playground/experimental/sim2sim/play_wolvesOP_joystick.py
@@ -86,6 +86,8 @@ class OnnxController:
     return obs.astype(np.float32)
 
   def get_control(self, model: mujoco.MjModel, data: mujoco.MjData) -> None:
+    global sensor_addr
+    
     self._counter += 1
     if self._counter % self._n_substeps == 0:
       obs = self.get_obs(model, data)
